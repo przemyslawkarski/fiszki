15270bd67d24a1e693a0defbb7bd06976407e898
Przemysław Karski <przemyslaw.karski85@gmail.com>
2026-02-20 21:25:22 +0100

    adding searchbox

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ cards.forEach((card) => {
     card.classList.toggle("flipped");
   });
 });
+
+const input = document.getElementById("searchInput");
+const sections = document.querySelectorAll(".card-container");
+
+input.addEventListener("input", function () {
+  const value = this.value.toLowerCase().trim();
+
+  sections.forEach(section => {
+    const text = section.textContent.toLowerCase();
+    section.style.display = text.includes(value) ? "block" : "none";
+  });
+});
